5dd935505bf6265b915a8d2b8f41e58f294634b4
qiuxinruo <qiuxr@shinemo.com>
2020-11-16 16:56:09 +0800

    fix

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -9,13 +9,13 @@ import { withRouter } from 'react-router-dom'
 const App = (props) => {
   console.log(props.location)
   const pathList  = props.location.pathname.split('/')
-  console.log(pathList.some(item=>item!=='login'||item!=='detail'))
+  console.log('223',pathList.some(item=>item!=='login'&&item!=='detail'))
   return (<div className='db_app'>
     {
       !pathList.some(item=>item==='login') && <Header />
     }
     {
-      pathList.some(item=>item!='login'||item!='detail') && < Nav />
+      pathList.some(item=>item!=='login'&&item!=='detail') && < Nav />
     }
     <div className={classnames('db_app_DashBoard-wrap')}>
       <DashBoard />
