1b0dcb0958e69194a6d59b33a58d6a6821758ad8
qiuxinruo <qiuxr@shinemo.com>
2020-11-16 17:06:38 +0800

    fix

--- src/main/app.tsx
+++ src/main/app.tsx
@@ -7,15 +7,13 @@ import classnames from 'classnames'
 import { withRouter } from 'react-router-dom'
 
 const App = (props) => {
-  console.log(props.location)
   const pathList  = props.location.pathname.split('/')
-  console.log('223',pathList.some(item=>item!=='login'&&item!=='detail'))
   return (<div className='db_app'>
     {
       !pathList.some(item=>item==='login') && <Header />
     }
     {
-      pathList.some(item=>item!=='login'&&item!=='detail') && < Nav />
+      !pathList.some(item=>item==='detail') &&!pathList.some(item=>item==='login') && < Nav />
     }
     <div className={classnames('db_app_DashBoard-wrap')}>
       <DashBoard />
